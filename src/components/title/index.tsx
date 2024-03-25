999a54140bb6f63a1f9ff37786cded4d0ba49d78
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-25 19:18:04 +0900

    chore: 불필요한 코드 제거

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,9 +1,4 @@
-import { useGetSearchPlace } from "@/hooks";
-
 const Title = () => {
-  const searchTerm = "강남";
-  const { data } = useGetSearchPlace({ searchTerm });
-  console.log("data", data);
   return (
     <div className="relative flex h-20 w-96 items-center justify-center">
       <span className="text-3xl font-bold italic text-green-600">Rest Spot Finder</span>
