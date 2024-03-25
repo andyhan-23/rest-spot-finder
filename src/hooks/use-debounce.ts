9620914b3570a757814b00c8a072f124bd30c08e
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-25 18:05:07 +0900

    feat: 디바운스 커스텀 훅 구현

---
+++ src/hooks/use-debounce.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = (value: string, delay: number = 200) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+export default useDebounce;
