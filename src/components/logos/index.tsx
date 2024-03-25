c8b22fe5978935205c0f5df95bec6ba5c0486a92
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2024-03-25 17:39:30 +0900

    feat: 로고 컴포넌트 구현 (#6)

---
+++ src/components/logos/index.tsx
@@ -0,0 +1,13 @@
+import NAVER_LOGO from "@/assets/naver_logo.png";
+
+const Logos = () => {
+  return (
+    <div className="relative bottom-0 left-0 top-0 border bg-white">
+      <i className="naverLogo flex h-16 w-16">
+        <img src={NAVER_LOGO} alt="naver_logo" />
+      </i>
+    </div>
+  );
+};
+
+export default Logos;
