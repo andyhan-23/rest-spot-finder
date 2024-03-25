8d97bb5c4374758990414a45cb026a5d059ea76d
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-25 16:39:33 +0900

    tailwind css 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2bca43",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
