dd4dfc9d1e65cd159984a233d1d4960c965268b9
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-27 02:33:22 +0900

    feat: 최근 검색 컴포넌트 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Title } from "./title";
 export { default as InputSubmit } from "./input-submit";
 export { default as Loading } from "./loading";
 export { default as PathInfo } from "./path-info";
+export { default as RecentSearch } from "./recent-search";
