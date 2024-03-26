dd5ba2c9a70dd834c3f7beaf0068a239a6ed37c9
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-27 02:53:43 +0900

    feat: 구간 별로 존재하는 휴게소 정보들을 데이터 패칭할수 있게 커스텀 훅으로 구현

--- src/apis/constant.ts
+++ src/apis/constant.ts
@@ -2,6 +2,7 @@ const URL = Object.freeze({
   base: "http://3.37.19.140:8080/api",
   searchPlace: "/place/naver?searchTerm",
   routes: "/route?start",
+  restSpots: "/restarea/route?routeId",
 } as const);
 
 export default URL;
