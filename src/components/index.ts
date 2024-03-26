2f3dcc34da76b7264b4296e7d2ddb77168a3922f
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2024-03-27 02:26:15 +0900

    feat: 경로 정보 컴포넌트 구현 (#16)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Logos } from "./logos";
 export { default as Title } from "./title";
 export { default as InputSubmit } from "./input-submit";
 export { default as Loading } from "./loading";
+export { default as PathInfo } from "./path-info";
