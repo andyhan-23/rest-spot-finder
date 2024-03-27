9a4ce0c1d35453ed211e98af39b01dc6096fbd82
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2024-03-27 15:13:34 +0900

    feat: 디자인 수정 (#21)

--- src/components/recent-search/index.tsx
+++ src/components/recent-search/index.tsx
@@ -2,12 +2,12 @@ import { LocationIcon } from "@/assets/icons";
 
 const RecentSearch = () => {
   return (
-    <div className="mt-12 w-80 bg-white">
+    <div className="mt-12 w-96 bg-white px-8">
       <div>
-        <h1 className="text-sm font-bold">최근검색</h1>
-        <h1 className="mx-auto mt-3 w-80 border-gray-200" />
+        <h1 className="text-xm font-bold">최근 검색</h1>
+        <hr className="mx-auto mt-3 w-80 border-gray-200" />
       </div>
-      <div className="felx-col flex">
+      <div className="flex flex-col">
         <div className="flex flex-row">
           <LocationIcon className="mr-2 mt-2" />
           <p className="mt-2 text-slate-800">가천대학교 글로벌 캠퍼스</p>
