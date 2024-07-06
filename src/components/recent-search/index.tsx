2f59dd780cfd937b1e3c8380a15e219a66767994
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-07-06 19:54:35 +0900

    chore: recentSearch 컴포넌트 디자인 tailwind css로 수정

--- src/components/recent-search/index.tsx
+++ src/components/recent-search/index.tsx
@@ -1,4 +1,3 @@
-import "./index.css";
 import { RecentSearchPropsType } from "@/types";
 
 const RecentSearch = ({
@@ -14,37 +13,50 @@ const RecentSearch = ({
   setClickedPlaceHistory,
 }: RecentSearchPropsType) => {
   return (
-    <div className="recentSearch">
-      <div className="title">
+    <div className="mt-[-1rem] px-6">
+      <div className="mt-8 flex justify-between border-b border-gray-200 pb-2 pl-2 text-sm font-semibold">
         <p>최근 검색한 장소</p>
-        <span onClick={() => clearHistory("place")}>검색 기록 삭제</span>
+        <span
+          onClick={() => clearHistory("place")}
+          className="hover:text-shadow mr-[-10px] flex cursor-pointer items-center justify-center px-2 text-xs text-red-500 hover:shadow-md"
+        >
+          검색 기록 삭제
+        </span>
       </div>
-      <div className="list place">
+      <div className="my-4 mb-12 text-sm font-semibold text-gray-800">
         {placeHistory.length > 0 ? (
           placeHistory
             .slice()
             .reverse()
-            .map(place => (
+            .map((place, index) => (
               <p
+                key={index}
                 onClick={() => {
                   setClickedPlaceHistory(true);
                   if (startPlace == null) setStartPlace(place);
                   else if (goalPlace == null) setGoalPlace(place);
                 }}
+                className="hover:text-shadow-md mx-2.5 my-4 cursor-pointer hover:text-blue-600"
               >
+                <span className="font-awesome pr-2 text-gray-400 before:content-['\f3c5']"></span>
                 {place.name}
               </p>
             ))
         ) : (
-          <span>검색 기록이 없습니다.</span>
+          <span className="text-shadow-md mx-2.5 my-4">검색 기록이 없습니다.</span>
         )}
       </div>
 
-      <div className="title">
+      <div className="mt-8 flex justify-between border-b border-gray-200 pb-2 pl-2 text-sm font-semibold">
         <p>최근 검색한 경로</p>
-        <span onClick={() => clearHistory("route")}>검색 기록 삭제</span>
+        <span
+          onClick={() => clearHistory("route")}
+          className="text-crimson hover:text-shadow mr-[-10px] flex cursor-pointer items-center justify-center px-2 text-xs text-red-500 hover:shadow-md"
+        >
+          검색 기록 삭제
+        </span>
       </div>
-      <div className="list route">
+      <div className="my-4 mb-12 text-sm font-semibold text-gray-800">
         {routeHistory.length > 0 ? (
           routeHistory
             .slice()
@@ -56,15 +68,18 @@ const RecentSearch = ({
                   setSelectedRouteHistory(routeHistory);
                   handleClickRecentSearch();
                 }}
+                className="hover:text-shadow-md mx-2.5 my-4 cursor-pointer hover:text-blue-600"
               >
+                <span className="font-awesome pr-2 text-gray-400 before:content-['\f1b9']"></span>
                 {routeHistory.name}
               </p>
             ))
         ) : (
-          <span>검색 기록이 없습니다.</span>
+          <span className="text-shadow-md mx-2.5 my-4">검색 기록이 없습니다.</span>
         )}
       </div>
     </div>
   );
 };
+
 export default RecentSearch;
