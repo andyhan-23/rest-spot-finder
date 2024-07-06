a667d83c16825d55cc759362959d19ecae14d07e
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-07-06 19:58:29 +0900

    chore: Title 컴포넌트 디자인 수정

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
-    <div className="relative flex h-20 w-96 items-center justify-center">
-      <span className="text-3xl font-bold italic text-green-600">경로상 휴게소 찾기</span>
+    <div className="block px-4 py-6 text-center">
+      <span className="text-2xl font-medium text-green-700">경로상 휴게소 찾기</span>
     </div>
   );
 };
